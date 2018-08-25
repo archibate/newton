42e785947beda0505cb2cce4767159055c3ded84
archibate <m17721388340@163.com>
2018-08-25 21:52:08 +0800

    Fixed demo/index.html: script src=...

--- src/AABB.js
+++ src/AABB.js
@@ -9,6 +9,8 @@ N2.AABB = Class.extend({
 
 	collide: function(c) {
 		// https://www.ibm.com/developerworks/cn/web/wa-build2dphysicsengine/
-		return this.b < c.t || this.t > c.b || this.r < c.l || this.l > c.r;
+		return  (! this.b < c.t || this.t > c.b
+			|| this.r < c.l || this.l > c.r
+			);
 	},
 });
